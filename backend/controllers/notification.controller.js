2ca202e04b28179ab35725c3e6a9793a8d563bab
omkar patil <omkarpatil14>
2024-08-17 23:53:50 +0530

    deleteOne notifiaction backend done

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,4 +1,5 @@
 import Notification from "../models/notification.model.js";
+import User from "../models/user.model.js";
 
 export const getNotifications = async (req, res) => {
 	try {
@@ -30,3 +31,27 @@ export const deleteNotifications = async (req, res) => {
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
+
+export const deleteOneNotification = async (req,res)=>{
+
+	try {
+		const NotificationId= req.params._id;
+		const userId= req.user._id;
+
+		const isNotification= Notification.findById({NotificationId})
+		if(!isNotification){
+			res.status(404).json({error:"notification not found"});
+
+		}
+
+		if(isNotification.to.toString()!==userId.toString()){
+			res.status(403).json({error:"user not authorized"})
+		}
+
+		await Notification.findByIdAndDelete(NotificationId);
+		res.status(200).json({message:"notification deleted sucessfully"})
+	} catch (error) {
+		console.log("Error in notification controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+}
